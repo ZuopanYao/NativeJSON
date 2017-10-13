dd946a1957aefa2b93c842c6164935d1694cbbd0
ZuopanYao <yaozuopan@icloud.com>
2017-10-13 16:33:31 +0800

    添加json使用示例

--- NativeJSON/Classes/AppDelegate.swift
+++ NativeJSON/Classes/AppDelegate.swift
@@ -15,9 +15,80 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        jsonBase()
+        jsonComplex()
+        
+        //============================ NOTE =====================================
+        // 1、模型需要继承Codable协议。
+        // 2、某些字段不确定是否有值的时，需要将模型中相应的字段定义为Optional, 这告诉JSONEncoder/JSONDecoder此字段没有值时不用拆包，否则将导致crash的发生。
+        // 3、模型嵌套模型时，所涉及到的模型都要继承Codable协议。
+        // 4、有时候json数据中的key不一定和我的们模型中的字段名一样，这时候我们就要做关联。如何关联请参考： UserModel
+        
         return true
     }
 
+    // 简单的json转换
+    func jsonBase() {
+        
+        // JSON编码 ---> 将[模型]转成JSON字符串
+        let user = UserModel(name: "Harvey", age: 18, email: "yaozuopan@icloud.com")
+        
+        if let jsonData = try? JSONEncoder().encode(user)  { // 编码成功
+            
+            if let jsonString = String(data: jsonData, encoding: String.Encoding.utf8) {
+                
+                print("----------------Base--------------------")
+                print(jsonString)
+            }
+        }
+        
+        // JSON解码 ---> 将JSON字符串转成[模型]
+        let jsonString = "{\"userName\":\"HarveyCC\",\"age\":20,\"userEmail\":\"yaozuopan@icloud.com\"}"
+        
+        if let jsonData = jsonString.data(using: String.Encoding.utf8) { // 解码成功
+            
+            if let user = try? JSONDecoder().decode(UserModel.self, from: jsonData) {
+                
+                print("----------------Base--------------------")
+                print(user.name, user.age, user.email)
+            }
+        }
+    }
+    
+    // 模型包含模型的json转换
+    func jsonComplex() {
+        
+        // JSON编码 ---> 将[模型]转成JSON字符串
+        let group = Department(name: "软件部", id: 889)
+        let user1 = UserModel(name: "Harvey", age: 18, email: "Harvey@icloud.com")
+        let user2 = UserModel(name: "Jojo", age: 25, email: "Jojo@icloud.com")
+        group.member = [user1, user2];
+        
+        if let jsonData = try? JSONEncoder().encode(group)  { // 编码成功
+            
+            if let jsonString = String(data: jsonData, encoding: String.Encoding.utf8) {
+                
+                print("----------------Complex--------------------")
+                print(jsonString)
+            }
+        }
+        
+        // JSON解码 ---> 将JSON字符串转成[模型]
+        let jsonString = "{\"name\":\"软件部\",\"id\":888,\"member\":[{\"userName\":\"Yoyo\",\"age\":18,\"userEmail\":\"Yoyo@icloud.com\"},{\"userName\":\"Jackly\",\"age\":25,\"userEmail\":\"Jackly@icloud.com\",\"qq\":\"6554866\"}]}"
+        
+        if let jsonData = jsonString.data(using: String.Encoding.utf8) { // 解码成功
+            
+            if let group = try? JSONDecoder().decode(Department.self, from: jsonData) {
+                
+                print("----------------Complex--------------------")
+                print(group.name, group.id, group.member.count)
+                print(group.member[0].name, group.member[0].age, group.member[0].email, group.member[0].qq ?? "nil")
+                print(group.member[1].name, group.member[1].age, group.member[1].email, group.member[1].qq ?? "nil")
+            }
+        }
+    }
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
--- NativeJSON/Model/UserModel.swift
+++ NativeJSON/Model/UserModel.swift
@@ -13,4 +13,33 @@ class UserModel: Codable {
     var name: String
     var age: Int
     var email: String
+    var qq: String? = nil     // Optional
+
+    init(name: String, age: Int, email: String) {
+        
+        self.name = name
+        self.age = age
+        self.email = email
+    }
+    
+    enum CodingKeys : String, CodingKey { // 有时候json数据中的key不一定和我的们模型中的字段名一样，这时候我们就要做关联
+                                          // 注意1:CodingKeys是固定的枚举的名称，不能自定义。
+        case name = "userName"            // 注意2:一旦定义了CodingKeys，JSONEncoder/JSONDecoder将根据CodingKeys去工作
+        case age                          //      没有在CodingKeys中的字段将被过滤掉了
+        case email = "userEmail"
+        case qq
+    }
+}
+
+class Department: Codable {
+    
+    var name: String
+    var id: Int
+    var member: [UserModel] = []
+    
+    init(name: String, id: Int) {
+        
+        self.name = name;
+        self.id = id;
+    }
 }
