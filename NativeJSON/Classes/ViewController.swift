1875c99df1962036385359f99e2931f7408281df
ZuopanYao <yaozuopan@icloud.com>
2017-10-13 10:51:14 +0800

    init

---
+++ NativeJSON/Classes/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  NativeJSON
+//
+//  Created by Harvey on 2017/10/13.
+//  Copyright © 2017年 Harvey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
---
+++ NativeJSON/Model/UserModel.swift
@@ -0,0 +1,16 @@
+//
+//  UserModel.swift
+//  NativeJSON
+//
+//  Created by Harvey on 2017/10/13.
+//  Copyright © 2017年 Harvey. All rights reserved.
+//
+
+import Foundation
+
+class UserModel: Codable {
+ 
+    var name: String
+    var age: Int
+    var email: String
+}
